02cd30b7769ac6ff5d134d89f3a49767246e103b
Oduntan Fawaz <oduntanfawaz@gmail.com>
2023-03-16 14:57:59 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,18 +5,19 @@
  *
  * Description: using sizeof to print the size of various types.
  *
- * Return: Always 0 (Sucess)
+ * Return: Always 0 (Sucessful)
  */
 
 int main(void)
 
 {
-printf("Size of a char variable: %lubyte(s)\n", sizeof(char));
-printf("Size of an int variable: %lubyte(s)\n", sizeof(int));
-printf("Size of a double variable: %lubyte(s)\n", sizeof(double));
-printf("Size of a long variable: %lubyte(s)\n", sizeof(long int));
-printf("Size of a long long variable: %lubyte(s)\n", sizeof(long long int));
-printf("Size of a float variable: %lubyte(s)\n", sizeof(float));
+	/*sideof evaluates the size of a variable*/
+printf("Size of a char: %lubyte(s)\n", sizeof(char));
+printf("Size of an int: %lubyte(s)\n", sizeof(int));
+printf("Size of a double: %lubyte(s)\n", sizeof(double));
+printf("Size of a long: %lubyte(s)\n", sizeof(long int));
+printf("Size of a long long: %lubyte(s)\n", sizeof(long long int));
+printf("Size of a float: %lubyte(s)\n", sizeof(float));
 
 return (0);
 }
