d805935ba929bf9aaa803659479416cbddca4ed6
Oduntan Fawaz <oduntanfawaz@gmail.com>
2023-03-18 11:20:08 -0700

    Updated Adv Task 0

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,13 +17,16 @@ int main(void)
 		{
 			putchar((i % 10) + '0');
 			putchar((j % 10) + '0');
+
 			if (i == 8 && j == 9)
 
-		continue;
+continue;
+
 			putchar(',');
 			putchar(' ');
 		}
 	}
 	putchar('\n');
+
 	return (0);
 }
