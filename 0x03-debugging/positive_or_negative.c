83701638fc8f37ba9708317fedba7da9bdccdf2d
Oduntan Fawaz <oduntanfawaz@gmail.com>
2023-03-24 05:23:46 -0700

    Updated Task 0

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include "main.h"
+
 /**
  * main - Entry point
  *
- * Description: prints value of n status; zero, positive or negative
+ * @i: parameter that holds interger to be tested
  *
  * Return: Always 0 (success)
  */
 
 void positive_or_negative(int i)
 {
+	srand(time(0));
+	i = rand() - RAND_MAX / 2;
+
 	if (i == 0)
 	{
 		printf("%d is zero\n", i);
