5e5020b3cdd5dc9e478b43c396bb9b713935db77
Oduntan Fawaz <oduntanfawaz@gmail.com>
2023-04-18 07:54:28 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - function name
+ * @name: parameter of char data type
+ * @age: parameter of float data type
+ * @owner: parameter of char data type
+ *
+ * Description: this function takes 3 members and print to screen
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
